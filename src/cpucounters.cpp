2a7ee6a234bca8c0c471dbe36fab77697a21436f
xiaoxiang wu <xiaoxiang.wu@hotmail.com>
2026-08-06 15:09:07 +0200

    Z490I

--- src/cpucounters.cpp
+++ src/cpucounters.cpp
@@ -7617,7 +7617,8 @@ ServerUncoreCounterState PCM::getServerUncoreCounterState(uint32 socket)
             {
                 result.CXLCMCounter[p][i] = *cxlPMUs[socket][p].first.counterValue[i];
 
-                if (xx)xxtrace("result.CXLCMCounter[%lu][%d] %llu",p,i,result.CXLCMCounter[p][i]);
+                if (xx)
+                    xxtrace("result.CXLCMCounter[%lu][%d] %llu",p,i,result.CXLCMCounter[p][i]);
             }
             for (int i = 0; i < ServerUncoreCounterState::maxCounters && socket < cxlPMUs.size() && size_t(i) < cxlPMUs[socket][p].second.size(); ++i)
             {
